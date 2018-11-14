3394f18a951dd3901a47c7fab7bf51a54b59fd72
Pillercottrer <emil.henning@gmail.com>
2018-11-14 22:47:38 +0100

    word_lstm etc

--- model_hierarchical.py
+++ model_hierarchical.py
@@ -19,7 +19,6 @@ class Encoder(nn.Module):
         # Remove linear and pool layers (since we're not doing classification)
         modules = list(resnet.children())[:-2]
         self.resnet = nn.Sequential(*modules)
-
         # Resize image to fixed size to allow input images of variable size
         self.adaptive_pool = nn.AdaptiveAvgPool2d((encoded_image_size, encoded_image_size))
 
@@ -76,8 +75,8 @@ class CoAttention(nn.Module):
         att1_visual = self.encoder_visual_attention(visual_features)
         att1_semantic = self.encoder_semantic_attention(semantic_features)
 
-        att_hidden_visual = self.hidden_dim_attention(hidden_lol)
-        att_hidden_semantic = self.hidden_dim_attention(hidden_lol)
+        att_hidden_visual = self.hidden_dim_attention_visual(hidden_lol)
+        att_hidden_semantic = self.hidden_dim_attention_semantic(hidden_lol)
 
         att_fullvisual = self.full_attention_visual(self.tanh(att1_visual + att_hidden_visual.unsqueeze(1))).squeeze(2)
         att_fullsemantic = self.full_attention_semantic(self.tanh(att1_semantic + att_hidden_semantic.unsqueeze(1))).squeeze(2)
@@ -95,7 +94,7 @@ class CoAttention(nn.Module):
 
 class MLC(nn.Module):
 
-    def __init__(self, num_pixels, encoder_dimension, tag_dimension):  #from features a single fully connected layer computes tags.
+    def __init__(self, num_pixels, encoder_dimension, tag_dimension, vocab_size, embed_dim):  #from features a single fully connected layer computes tags.
         print('lalal')
         super(MLC, self).__init__()
         self.encoder_dimension = encoder_dimension
@@ -106,77 +105,50 @@ class MLC(nn.Module):
 
         self.softmax = nn.Softmax(dim = 1) # 1?
 
+        self.embedding = nn.Embedding(vocab_size, embed_dim)  # embedding layer
+
+        # Ändra till rtg-nätverk sen.
+        resnet = torchvision.models.resnet101(pretrained=True)  # pretrained ImageNet ResNet-101
+
+        # Remove linear and pool layers (since we're not doing classification)
+        modules = list(resnet.children())[-2:]
+        self.resnet_avgpool = modules[0]
+        self.resnet_fc= modules[1]
+
     def init_weights(self):
         """
         Initializes some parameters with values from the uniform distribution, for easier convergence.
         """
         self.mlc.bias.data.fill_(0)
         self.mlc.weight.data.uniform_(-0.1, 0.1)
+        self.embedding.weight.data.uniform_(-0.1, 0.1)
+
 
     def forward(self, visual_features):
-        print('qweqweqweqweqweqweqwe')
-        mlc_encoding = visual_features.contiguous().view(1, self.encoder_dimension*self.num_pixels)
+        """Använda 2 sista lagren av resnet istället???"""
+        #test = self.resnet_avgpool(visual_features)
+        #test = test.squeeze(3)
+        #test = test.squeeze(2)
+
+        mlc_encoding = visual_features.contiguous().view(-1, self.encoder_dimension*self.num_pixels)
         tags = self.mlc(mlc_encoding)
         tags_softmax = self.softmax(tags) #????
-        return tags_softmax
+
+        sorted, indices = torch.sort(tags_softmax, descending=True)
+
+
+        return indices[:,:5]
 
 
 
+class Embedding(nn.Module):
+    def __init__(self, vocab_size, embed_dim):
+        super(Embedding, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_dim)
+        self.init_weights()
 
-class SentenceLSTMDecoder(nn.Module):
-
-    def __init__(self, attention_dim, embed_dim, decoder_dim, vocab_size, encoder_visual_dim=2048, encoder_semantic_dim=2048, ctx_dim = 2048, dropout=0.5):
-        """
-        :param attention_dim: size of attention network
-        :param embed_dim: embedding size
-        :param decoder_dim: size of decoder's RNN
-        :param vocab_size: size of vocabulary
-        :param encoder_dim: feature size of encoded images
-        :param dropout: dropout
-        """
-
-
-        #TODO
-        #double check.
-        super(SentenceLSTMDecoder, self).__init__()
-
-        self.encoder_visual_dim = encoder_visual_dim
-        self.encoder_semantic_dim = encoder_semantic_dim
-        self.ctx_dim = ctx_dim
-        self.attention_dim = attention_dim  #dimension of ctx vector
-        self.embed_dim = embed_dim  #word embedding?
-        self.decoder_dim = decoder_dim  #hidden state dim?
-        self.vocab_size = vocab_size
-        self.dropout = dropout
-
-        self.attention = CoAttention(encoder_visual_dim, encoder_semantic_dim, ctx_dim, decoder_dim, attention_dim)  # attention network
-
-        self.embedding = nn.Embedding(vocab_size, embed_dim)  # embedding layer
-        self.dropout = nn.Dropout(p=self.dropout)
-        self.decode_step = nn.LSTMCell(embed_dim + ctx_dim, decoder_dim, bias=True)  # decoding LSTMCell
-        self.init_h = nn.Linear(ctx_dim, decoder_dim)  # linear layer to find initial hidden state of LSTMCell
-        self.init_c = nn.Linear(ctx_dim, decoder_dim)  # linear layer to find initial cell state of LSTMCell
-
-        #behövs dessa?
-        self.f_beta = nn.Linear(decoder_dim, ctx_dim)  # linear layer to create a sigmoid-activated gate
-        self.sigmoid = nn.Sigmoid()
-        self.fc = nn.Linear(decoder_dim, vocab_size)  # linear layer to find scores over vocabulary
-        self.init_weights()  # initialize some layers with the uniform distribution
-
-    def init_weights(self):
-        """
-        Initializes some parameters with values from the uniform distribution, for easier convergence.
-        """
-        self.embedding.weight.data.uniform_(-0.1, 0.1)
-        self.fc.bias.data.fill_(0)
-        self.fc.weight.data.uniform_(-0.1, 0.1)
-
-    def load_pretrained_embeddings(self, embeddings):
-        """
-        Loads embedding layer with pre-trained embeddings.
-        :param embeddings: pre-trained embeddings
-        """
-        self.embedding.weight = nn.Parameter(embeddings)
+    def forward(self, words):
+        return self.embedding(words)
 
     def fine_tune_embeddings(self, fine_tune=True):
         """
@@ -186,19 +158,128 @@ class SentenceLSTMDecoder(nn.Module):
         for p in self.embedding.parameters():
             p.requires_grad = fine_tune
 
-    def init_hidden_state(self, encoder_out):
+    def init_weights(self):
+        """
+        Initializes some parameters with values from the uniform distribution, for easier convergence.
+        """
+        self.embedding.weight.data.uniform_(-0.1, 0.1)
+
+    def load_pretrained_embeddings(self, embeddings):
+        """
+        Loads embedding layer with pre-trained embeddings.
+        :param embeddings: pre-trained embeddings
+        """
+        self.embedding.weight = nn.Parameter(embeddings)
+
+
+
+
+class SentenceLSTMDecoder(nn.Module):
+
+    def __init__(self, vocab_size, num_pixels=196, ctx_attention_dim=512, semantic_embed_dim = 512, hidden_dim=512, encoder_visual_dim=2048, ctx_dim = 100, mlc_dim = 15, t_dim = 111, stop_dim=100, max_sents=8, dropout=0.5):
+        """
+        :param attention_dim: size of attention network
+        :param embed_dim: embedding size
+        :param decoder_dim: size of decoder's RNN
+        :param vocab_size: size of vocabulary
+        :param encoder_dim: feature size of encoded images
+        :param dropout: dropout
+        """
+        #double check.
+        super(SentenceLSTMDecoder, self).__init__()
+
+        self.encoder_visual_dim = encoder_visual_dim
+        self.ctx_dim = ctx_dim
+        self.attention_dim = ctx_attention_dim  #dimension of ctx vector
+        self.semantic_embed_dim = semantic_embed_dim  #word embedding?
+        self.hidden_dim = hidden_dim  #hidden state dim?
+        self.vocab_size = vocab_size
+        self.dropout = dropout
+
+        self.attention = CoAttention(encoder_visual_dim, semantic_embed_dim, ctx_dim, hidden_dim, ctx_attention_dim)  # attention network
+
+        self.embedding = nn.Embedding(vocab_size, semantic_embed_dim)  # embedding layer
+        self.dropout = nn.Dropout(p=self.dropout)
+        self.lstm_sentence = nn.LSTMCell(ctx_dim, hidden_dim, bias=True)  # decoding LSTMCell
+        self.init_h = nn.Linear(ctx_dim, hidden_dim)  # linear layer to find initial hidden state of LSTMCell
+        self.init_c = nn.Linear(ctx_dim, hidden_dim)  # linear layer to find initial cell state of LSTMCell
+
+        #behövs dessa?
+        self.f_beta = nn.Linear(hidden_dim, ctx_dim)  # linear layer to create a sigmoid-activated gate
+        self.sigmoid = nn.Sigmoid()
+        self.fc = nn.Linear(hidden_dim, vocab_size)  # linear layer to find scores over vocabulary
+
+
+        #new stuff
+        self.max_sentences = max_sents
+        self.mlc_dim = mlc_dim
+        self.t_dim = t_dim
+        self.mlc = MLC(num_pixels, encoder_visual_dim, self.mlc_dim, self.vocab_size, self.semantic_embed_dim)  # visual_attention dim, encoder_dim, tag_dim. vocab_size, embed_dim
+
+        self.fc_h_to_t = nn.Linear(hidden_dim, t_dim)
+        self.fc_ctx_to_t = nn.Linear(ctx_dim, t_dim)
+        self.tanh = nn.Tanh()
+
+        self.stop_control_1 = nn.Linear(self.hidden_dim, stop_dim) #h(t-1)
+        self.stop_control_2 = nn.Linear(self.hidden_dim, stop_dim) # h(t)
+        self.stop_control_3 = nn.Linear(stop_dim, 2) #
+
+        self.softmax = nn.Softmax(dim=1)  # softmax layer to calculate weights
+
+        self.init_weights()  # initialize some layers with the uniform distribution
+
+
+
+    def fine_tune_embeddings(self, fine_tune=True):
+        """
+        Allow fine-tuning of embedding layer? (Only makes sense to not-allow if using pre-trained embeddings).
+        :param fine_tune: Allow?
+        """
+        for p in self.embedding.parameters():
+            p.requires_grad = fine_tune
+
+    def init_weights(self):
+        """
+        Initializes some parameters with values from the uniform distribution, for easier convergence.
+        """
+        self.embedding.weight.data.uniform_(-0.1, 0.1)
+        self.fc.bias.data.fill_(0)
+        self.fc.weight.data.uniform_(-0.1, 0.1)
+
+        self.fc_h_to_t.bias.data.fill_(0)
+        self.fc_h_to_t.weight.data.uniform_(-0.1, 0.1)
+        self.fc_ctx_to_t.bias.data.fill_(0)
+        self.fc_ctx_to_t.weight.data.uniform_(-0.1, 0.1)
+
+    def load_pretrained_embeddings(self, embeddings):
+        """
+        Loads embedding layer with pre-trained embeddings.
+        :param embeddings: pre-trained embeddings
+        """
+        self.embedding.weight = nn.Parameter(embeddings)
+
+
+    def init_hidden_state(self, batch_size):
         """
         Creates the initial hidden and cell states for the decoder's LSTM based on the encoded images.
         :param encoder_out: encoded images, a tensor of dimension (batch_size, num_pixels, encoder_dim)
         :return: hidden state, cell state
         """
+        """
         mean_encoder_out = encoder_out.mean(dim=1)
         h = self.init_h(mean_encoder_out)  # (batch_size, decoder_dim)
         c = self.init_c(mean_encoder_out)
+        """
+        h = torch.zeros(batch_size, self.hidden_dim).to(device)
+        c = torch.zeros(batch_size, self.hidden_dim).to(device)
         return h, c
 
     def forward(self, encoder_out):
-        print('lol')
+        # Multi label classification network
+        self.mlc.init_weights()
+        tags = self.mlc(encoder_out)
+
+        #set batch-size and encoder_dim
         batch_size = encoder_out.size(0)
         encoder_dim = encoder_out.size(-1)
 
@@ -206,6 +287,106 @@ class SentenceLSTMDecoder(nn.Module):
         encoder_out = encoder_out.view(batch_size, -1, encoder_dim)  # (batch_size, num_pixels, encoder_dim)
         num_pixels = encoder_out.size(1) #number of visual feature vectors
 
-        #Co-Attention
-        #fixa
+        # Multi label classification network
+        self.mlc.init_weights()
+        tags = self.mlc(encoder_out)
+
+        #semantic attention
+        a_att = self.embedding(tags)
+
+        # init_hidden_state
+        h, c = self.init_hidden_state(batch_size)  #h_0, c_0
+        h_last_time_step = h
+
+
+        topic_vectors = []
+        stop_vectors = []
+        for i in range(self.max_sentences):
+            # Co-Attention
+            ctx = self.attention(encoder_out, a_att, h)  # generate context vector from features and hidden state
+            h, c = self.lstm_sentence(ctx, (h, c)) #advance one time-step
+            stop = self.softmax(self.tanh(self.stop_control_3(self.stop_control_1(h_last_time_step)+self.stop_control_2(h))))
+            stop_vectors.append(stop)
+            t = self.tanh(self.fc_h_to_t(h) + self.fc_ctx_to_t(ctx))
+            topic_vectors.append(t)
+            h_last_time_step = h
+            print(stop)
+
+        topic_tensor = torch.Tensor(len(topic_vectors), self.t_dim)
+        topic_tensor = torch.cat(topic_vectors)
+
+        stop_tensor = torch.Tensor(len(stop_vectors), 2)
+        stop_tensor = torch.cat(stop_vectors)
+
+        return topic_tensor, stop_tensor
+
+class WordLSTMDecoder(nn.Module):
+    def __init__(self, vocab_size, topic_vector_size = 111, num_layers = 1, embed_size = 512, hidden_size = 512, max_seq_length=20):
+        super(WordLSTMDecoder, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTMCell(topic_vector_size, hidden_size, bias=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.max_seg_length = max_seq_length
+
+        self.init_h = nn.Linear(topic_vector_size, hidden_size)
+        self.init_c = nn.Linear(topic_vector_size, hidden_size)
+
+    def init_hidden_state(self, batch_size):
+        #h = self.init_h(topic_vectors)
+        #c = self.init_c(topic_vectors)
+        h = torch.zeros(batch_size, self.hidden_dim).to(device)
+        c = torch.zeros(batch_size, self.hidden_dim).to(device)
+        return h, c
+
+    def forward(self, topic_vectors, num_sents, paragraphs, sentence_lengths):
+        """Decode topic vectors and generates captions."""
+        """Topic vectors: 3D tensor (batch_size, num_topic vectors, )"""
+        """Number of sentences is a 1d tensor (batch size, 1)"""
+        """Paragraphs is a 3d tensor of  (batch size, max_sentence_length, max_sents)"""
+        """Sentence lengths is a 2d tensor (batch size, max_sents)"""
+
+        batch_size = topic_vectors.size(0)
+        topic_vector_size = topic_vectors.size(1)
+        topic_vector_dim = topic_vectors.size(-1)
+        vocab_size = self.vocab_size
+
+        num_sents, sort_ind = num_sents.sort(dim=0, descending=True)
+
+        outputs = 'a'
+        for i in range(topic_vector_size):  #Generate sentence from all topic_vectors
+            topic = topic_vectors[:,i]
+            captions = paragraphs[:,i,:]
+            lengths = sentence_lengths[:,i]
+
+            # Sort input data by decreasing lengths; why? apparent below (joke, its not so apparent)
+            caption_lengths, sort_ind = lengths.sort(dim=0, descending=True)
+            topic = topic[sort_ind]
+            captions = captions[sort_ind]
+
+            # Embedding
+            embeddings = self.embedding(captions)  # (batch_size, max_caption_length, embed_dim)
+
+            # Initialize LSTM state
+            h, c = self.init_hidden_state(batch_size)  # (batch_size, decoder_dim)
+
+            # We won't decode at the <end> position, since we've finished generating as soon as we generate <end>
+            # So, decoding lengths are actual lengths - 1
+            decode_lengths = (caption_lengths - 1).tolist()
+
+            # Create tensors to hold word predicion scores and alphas
+            predictions = torch.zeros(batch_size, max(decode_lengths), vocab_size).to(device)
+
+            for j in range(self.max_seg_length):
+                print('test')
+
+
+
+        return outputs
+
+
+
+
+
+
+
 
--- testing_stuff.py
+++ testing_stuff.py
@@ -1,9 +1,12 @@
 import torch
 import numpy as np
 import torchvision.transforms as transforms
+import pickle
+import nltk
 from skimage.transform import resize
 from imageio import imread
-from model_hierarchical import Encoder, CoAttention, MLC
+from model_hierarchical import Encoder, CoAttention, MLC, SentenceLSTMDecoder, Embedding, WordLSTMDecoder
+from build_vocab import Vocabulary
 
 
 
@@ -13,6 +16,41 @@ print('hello world')
 
 
 image_path = './000000000139.jpg'
+paragraphs = ['A little dog', 'A dog jumping over a fence', 'One small step for mankind']
+# Load vocabulary wrapper
+with open('./vocab.pkl', 'rb') as f:
+    vocab = pickle.load(f)
+
+paragraph_tokens =[]
+
+for sent in paragraphs:
+    tokens = nltk.tokenize.word_tokenize(str(sent).lower())
+    print(tokens)
+    caption = []
+    caption.append(vocab('<start>'))
+    caption.extend([vocab(token) for token in tokens])
+    caption.append(vocab('<end>'))
+    caption = torch.Tensor(caption)
+    paragraph_tokens.append(caption)
+
+
+
+
+lengths = [len(sent) for sent in paragraph_tokens]
+
+target_paragraphs = torch.zeros(1, len(paragraph_tokens), max(lengths)).long()
+
+for i, sent_tokens in enumerate(paragraph_tokens):
+    target_paragraphs[0, i,:lengths[i]] = sent_tokens
+
+paragraph_sent_lengths = torch.Tensor(lengths)
+paragraph_sent_lengths = paragraph_sent_lengths.unsqueeze(0)
+
+num_sents = torch.Tensor([3])
+num_sents = num_sents.unsqueeze(0)
+
+
+
 
 # Read image and process
 img = imread(image_path)
@@ -29,6 +67,15 @@ transform = transforms.Compose([normalize])
 image = transform(img)  # (3, 256, 256)
 
 
+
+#Constants
+mlc_dim = 25 # num of tags
+vocab_size = 11111 # random value for testing
+semantic_att_embed_size = 100
+
+
+
+
 #Encoding
 encoder = Encoder()
 encoder = encoder.to(device)
@@ -38,18 +85,52 @@ encoder_out = encoder(image)  # (1, enc_image_size, enc_image_size, encoder_dim)
 enc_image_size = encoder_out.size(1)
 encoder_dim = encoder_out.size(3)
 
+#Testing sentence topic generator (håll tummarna)
+sent_lstm = SentenceLSTMDecoder(vocab_size)
+sent_lstm.to(device)
+topic_vectors, stop_vectors = sent_lstm(encoder_out)
+
+#topic_tensor = torch.Tensor(len(topic_vectors), 111)
+#topic_tensor = torch.cat(topic_vectors)
+#topic_tensor = topic_tensor.unsqueeze(0)
+
+topic_vectors = topic_vectors.unsqueeze(0)
+
+word_lstm = WordLSTMDecoder(vocab_size)
+word_lstm.to(device)
+word_lstm(topic_vectors, target_paragraphs, num_sents, paragraph_sent_lengths)
+
+
+
+
+
+
+
 # Flatten encoding
 encoder_out = encoder_out.view(1, -1, encoder_dim)  # (1, num_pixels, encoder_dim)
 num_pixels = encoder_out.size(1)
 
 #Multi label classification network
-mlc = MLC(num_pixels,encoder_dim, 10)
+mlc = MLC(num_pixels,encoder_dim, mlc_dim, vocab_size, semantic_att_embed_size) #visual_attention dim, encoder_dim, tag_dim. vocab_size, embed_dim
 mlc = mlc.to(device)
 
 mlc.init_weights()
 tags = mlc(encoder_out)
 
+#Embed words
+embed = Embedding(vocab_size, semantic_att_embed_size)
+embed = embed.to(device)
+a_att = embed(tags)
+
 
 #Co-Attention
-coAttention = CoAttention(num_pixels, 10, 100, 512, 100)
+h = torch.zeros(512).to(device)
+h = h.unsqueeze(0)
+
+#visual_features = encoder_out.contiguous().view(-1, encoder_dim * num_pixels)
+
+coAttention = CoAttention(encoder_dim, semantic_att_embed_size, 100, 512, 100)
+coAttention.to(device)
+ctx = coAttention(encoder_out, a_att, h)
+
 
